d50406ff9c4ca656b316f42379e20b0e22a3892e
thquyen11 <thquyen11@hotmail.com>
2018-07-27 08:49:26 -0400

    update

---
+++ config.js
@@ -0,0 +1,11 @@
+VS CODE Setting
+
+{
+    "terminal.integrated.shell.windows": "C:\\Users\\QuyenH\\AppData\\Local\\Programs\\Git\\bin\\bash.exe",
+    "workbench.colorTheme": "Monokai",
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "explorer.confirmDragAndDrop": false
+}
+
+@installed
+DotENV highlight-matching-tag LiveServer Sublime Babel
