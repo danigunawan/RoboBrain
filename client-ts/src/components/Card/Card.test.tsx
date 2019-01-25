ad8b204c312b73df7ba6c3fdf61625a74e0cc102
thquyen11 <thquyen11@hotmail.com>
2019-01-26 04:57:45 +0700

    re-structure repo, remove client-ts and server

--- client-ts/src/components/Card/Card.test.tsx
+++
@@ -1,8 +0,0 @@
-// import {Card} from './Card';
-// import { shallow, mount, render } from 'enzyme';
-// import * as React from 'react';
-
-// it('expect to render Card componnet',()=>{
-//     expect(shallow(<Card />).length).toEqual(1);
-// })
-
--- client/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import configureMockStore from 'redux-mock-store';
-import thunkMiddleware from 'redux-thunk';
-
-configure({ adapter: new Adapter() });
-export const mockStore = configureMockStore([thunkMiddleware]);
